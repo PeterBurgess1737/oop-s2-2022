107bd5c3fd9e9ea464bb21f25378b758590ba520
Peter Burgess <pburgess2003@gmail.com>
2022-10-24 18:36:19 +1030

    ws7

--- practical-03/function-1-3.cpp
+++
@@ -1,7 +0,0 @@
-void copy_integers(int old_array[], int new_array[], int length)
-{
-    for (int offset = 0; offset < length; offset++)
-    {
-        *(new_array + offset) = *(old_array + offset);
-    }
-}
