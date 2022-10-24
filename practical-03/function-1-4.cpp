30660f2ab92fc12b70836bc4828c80509d3e8ad1
Peter Burgess <pburgess2003@gmail.com>
2022-10-24 18:27:48 +1030

    ws1

--- practical-03/function-1-4.cpp
+++
@@ -1,7 +0,0 @@
-void copy_doubles(double *old_array, double *new_array, int length)
-{
-    for (int offset = 0; offset < length; offset++)
-    {
-        *(new_array + offset) = *(old_array + offset);
-    }
-}
