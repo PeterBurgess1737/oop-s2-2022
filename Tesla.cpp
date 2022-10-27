d18a03e41821d44ce7fd92ce9ff838047b9086e5
Peter Burgess <pburgess2003@gmail.com>
2022-10-27 15:40:08 +1030

    2-1 Tesla done i think

---
+++ Tesla.cpp
@@ -0,0 +1,60 @@
+#include "Tesla.h"
+
+int Tesla::nextVinNumber = 1000001;
+
+Tesla::Tesla() : Car(1)
+{
+    // Setting vinNumber
+    vinNumber = nextVinNumber;
+    nextVinNumber++;
+}
+
+Tesla::Tesla(char model, int price) : Car(price)
+{
+    this->model = model;
+
+    // Setting vinNumber
+    vinNumber = nextVinNumber;
+    nextVinNumber++;
+}
+
+void Tesla::chargeBattery(int mins)
+{
+    batteryPercentage += 0.5 * (float)mins;
+    if (batteryPercentage > 100.f)
+        batteryPercentage = 100.f;
+}
+
+void Tesla::drive(int kms)
+{
+    // Find number of kms possible to drive
+    // Then find emmisions
+
+    // Finding battery usage
+    float battery_usage = (float)kms / 5;
+
+    // Clamping it to the battery percentage
+    if (battery_usage > batteryPercentage)
+        battery_usage = batteryPercentage;
+
+    // Find emisions
+    emissions += battery_usage * 74;
+}
+
+void Tesla::set_model(char new_model)
+{
+    model = new_model;
+}
+char Tesla::get_model() const
+{
+    return model;
+}
+
+void Tesla::set_batteryPercentage(float new_batteryPercentage)
+{
+    batteryPercentage = new_batteryPercentage;
+}
+float Tesla::get_batteryPercentage() const
+{
+    return batteryPercentage;
+}
---
+++ Tesla.h
@@ -0,0 +1,43 @@
+#ifndef PRAC_EXAM_4_TESLA_H
+#define PRAC_EXAM_4_TESLA_H
+
+#include "Car.h"
+
+class Tesla : public Car
+{
+private:
+    // Used to set the vinNumber when a Tesla object is created
+    // Initialised to 1000001
+    static int nextVinNumber;
+
+    // The model of the car
+    char model;
+
+    // Initially 100%
+    float batteryPercentage = 100.f;
+
+public:
+    // Ze Constructorz
+    Tesla();
+    Tesla(char model, int price);
+
+    // The chargeBattery() method will add 0.5% to the batteryPercentage
+    // attribute for every minute charged, but cannot exceed 100% charged
+    void chargeBattery(int mins);
+
+    // Drive
+    // Uses 74g of CO2 per kilometre
+    // Uses 1% battery for every 5km driven
+    // Cannot continue driving when the battery percentage is at 0 therefore
+    // Will not emit CO2 if the battery percentage is at 0
+    void drive(int kms);
+
+    // Getters and Setters
+    void set_model(char new_model);
+    char get_model() const;
+
+    void set_batteryPercentage(float new_batteryPercentage);
+    float get_batteryPercentage() const;
+};
+
+#endif // PRAC_EXAM_4_TESLA_H
