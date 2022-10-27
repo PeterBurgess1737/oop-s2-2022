0bc1e20b95f073d74dce53c54c955a5e51fbc4f5
Peter Burgess <pburgess2003@gmail.com>
2022-10-27 15:17:22 +1030

    Question 1

---
+++ Car.h
@@ -0,0 +1,27 @@
+#ifndef PRAC_EXAM_4_CAR_H
+#define PRAC_EXAM_4_CAR_H
+
+class Car
+{
+protected:
+
+    // Price of the car
+    int price;
+
+    //
+    int vinNumber;
+
+    // CO2 emissions
+    // The emissions attribute will always start at 0
+    int emissions;
+
+public:
+    // Ze Constructor
+    Car(int price);
+
+    // In the derived classes, it will drive the car a certain amount of
+    // kilometres and will update the emissions attribute.
+    virtual void drive(int kms) = 0;
+};
+
+#endif // PRAC_EXAM_4_CAR_H
---
+++ main-1-1.cpp
@@ -0,0 +1,6 @@
+#include "Car.h"
+
+int main(void)
+{
+    return 0;
+}
