81be1eea0e1913e21853b0019e020a7c8bf4407c
Peter Burgess <pburgess2003@gmail.com>
2022-10-27 15:47:53 +1030

    Proper main file for 1-1

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -1,6 +1,16 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
 #include "Car.h"
 
 int main(void)
 {
+    cout << "\n========== Program Start ==========\n"
+         << endl;
+
+    cout << "\n=========== Program End ===========\n"
+         << endl;
+
     return 0;
 }
